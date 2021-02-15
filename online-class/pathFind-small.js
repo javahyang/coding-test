504f1b42c95c752dc308fca72ca48cbc69c82210
javahyang <javahyang@gmail.com>
2021-02-15 23:30:40 +0900

    오류수정, .slice() 로 현재까지 배열 push

--- online-class/pathFind-small.js
+++ online-class/pathFind-small.js
@@ -7,22 +7,29 @@ function solution(n, arr) {
   }, g);
   let count = 0;
   let check = Array(n + 1).fill(0);
+  let path = [];
+  let tmp = [];
   function DFS(V) {
     if (V === n) {
       count++;
+      path.push(tmp.slice());
       return;
     }
     g[V].map((item, idx) => {
-      if (check[idx]) return;
-      if (item) {
-        check[V + 1] = 1;
-        DFS(V + 1);
-        check[V + 1] = 0;
+      if (idx === 0) return;
+      if (item && check[idx] === 0) {
+        check[idx] = 1;
+        tmp.push(idx);
+        DFS(idx);
+        check[idx] = 0;
+        tmp.pop();
       }
     });
   }
+  tmp.push(1);
   check[1] = 1;
   DFS(1);
+  console.log(path);
   return count;
 }
 
