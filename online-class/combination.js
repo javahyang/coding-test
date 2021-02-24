70e6041e30607eaede3eef42a626b79c46d7e508
javahyang <javahyang@gmail.com>
2021-02-24 23:44:30 +0900

    DFS 속도향상 방법

--- online-class/combination.js
+++ online-class/combination.js
@@ -3,6 +3,7 @@ function solution(n, r) {
   // console.log(memo);
   function DFS(n, r) {
     if (r === 0 || n === r) return 1;
+    // 한 번 방문한 값은 저장한 후 접근
     else if (memo[n - 1][r - 1] > 0) {
       console.log("memo", memo[n - 1][r - 1]);
       return memo[n - 1][r - 1];
