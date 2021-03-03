8f32c7f45d08c7c2ed5d3a584a300f0b25abaf44
javahyang <javahyang@gmail.com>
2021-03-03 23:36:11 +0900

    배열의 중복 제거할 때 Set 이용

---
+++ programmers/폰켓몬.js
@@ -0,0 +1,7 @@
+function solution(nums) {
+  return Math.min(parseInt(nums.length / 2), new Set(nums).size);
+}
+
+console.log(solution([3, 1, 2, 3]));
+console.log(solution([3, 3, 3, 2, 2, 4]));
+console.log(solution([3, 3, 3, 2, 2, 2]));
