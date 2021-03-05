2bc89ea63b538f680f340ef81c698b598af12d60
javahyang <javahyang@gmail.com>
2021-03-05 23:16:35 +0900

    두 배열의 중복 체크할 때 filter, indexOf

---
+++ programmers/체육복.js
@@ -0,0 +1,29 @@
+function solution(n, lost, reserve) {
+  const ownClothes = n - lost.length;
+  let ownSpare = reserve.filter((item) => lost.indexOf(item) > -1).length;
+  console.log("자기가 챙겨온 여벌:", ownSpare);
+  if (ownSpare === lost.length) return n;
+
+  let noSpare = lost.filter((item) => reserve.indexOf(item) < 0);
+  console.log("빌려입어야 하는 사람:", noSpare);
+  let otherSpare = reserve.filter((item) => lost.indexOf(item) < 0);
+  console.log("이용할수 있는 여벌:", otherSpare);
+  let result = otherSpare.reduce((acc, curr) => {
+    if (noSpare.indexOf(curr) > -1) {
+      noSpare.splice(noSpare.indexOf(curr), 1);
+      acc.add(curr);
+    } else if (curr - 1 >= 1 && noSpare.indexOf(curr - 1) > -1) {
+      noSpare.splice(noSpare.indexOf(curr - 1), 1);
+      acc.add(curr - 1);
+    } else if (curr + 1 <= n && noSpare.indexOf(curr + 1) > -1) {
+      noSpare.splice(noSpare.indexOf(curr + 1), 1);
+      acc.add(curr + 1);
+    }
+    return acc;
+  }, new Set());
+  console.log("빌려입은 옷:", Array.from(result));
+  return ownClothes + ownSpare + result.size;
+}
+
+console.log(solution(5, [2, 3, 4], [1, 2, 3])); // 4
+console.log(solution(3, [3], [1])); // 2
