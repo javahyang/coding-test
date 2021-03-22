01e2c08aff050a968c4da480da013ac11c577f85
javahyang <javahyang@gmail.com>
2021-03-22 09:25:46 +0900

    3진법 변형

--- programmers/124나라의숫자.js
+++ programmers/124나라의숫자.js
@@ -1,3 +1,23 @@
-function solution(n) {}
+function solution(n) {
+  let answer = [];
+  function DFS(n) {
+    const share = parseInt(n / 3);
+    const rest = n % 3;
+    answer.unshift(rest == 0 ? 4 : rest);
 
-console.log(solution(13)); // 11
+    if (share < 3) {
+      if (share > 0) {
+        if (rest != 0) answer.unshift(share);
+        else if (rest == 0 && share - 1 != 0) answer.unshift(share - 1);
+      }
+      return;
+    }
+    DFS(rest == 0 ? share - 1 : share);
+  }
+
+  DFS(n);
+  return answer.join("");
+}
+
+console.log(solution(13)); // 111
+console.log(solution(273)); // 24244
